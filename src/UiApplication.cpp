daba52db6d914e0793cd4765e20cf9229a4917b5
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-11-30 14:33:47 -0600

    feat: Add the UiApplication source

---
+++ src/UiApplication.cpp
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiApplication.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "UiApplication.h"
+
+UiApplication::UiApplication(int argc, char **argv)
+{
+}
+
+UiApplication::UiApplication()
+{
+}
+
+UiApplication::~UiApplication()
+{
+}
