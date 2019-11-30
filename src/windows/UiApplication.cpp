f0c198af7659b271e45b0a1fc83f72eb444b2d53
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-11-30 14:34:48 -0600

    feat: Add the UiApplication (Windows) source

---
+++ src/windows/UiApplication.cpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiApplication.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "UiApplication.h"
+
+#include <windows.h>
+
+/* static */ int UiApplication::run(Window * window)
+{
+  MSG msg;
+
+  BOOL getMessageReturn;
+
+  while((getMessageReturn = ::GetMessageW(&msg, NULL, 0, 0)) != 0)
+  {
+    if(getMessageReturn == -1)
+    {
+      /*
+       * TODO: Handle error and possibly exit. Think about giving the user
+       * an option to exit on errors by changing the Application::run
+       * signature.
+       */
+    }
+    else
+    {
+      ::TranslateMessage(&msg);
+      ::DispatchMessageW(&msg);
+    }
+  }
+
+  return static_cast<int>(msg.wParam);
+}
