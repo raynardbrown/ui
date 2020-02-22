d58c71dc15d205c7b192076ac05327caeb367ed2
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-02-21 22:42:09 -0600

    feat: Add the TopLevelWindowPrivate header

---
+++ src/ui/private/windows/TopLevelWindowPrivate.h
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: TopLevelWindowPrivate.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_PRIVATE_WINDOWS_TOPLEVELWINDOWPRIVATE_H_
+#define UI_PRIVATE_WINDOWS_TOPLEVELWINDOWPRIVATE_H_
+
+#include <memory>
+
+#include "ui/private/UiComponentPrivate.h"
+
+class MenuBar;
+class TopLevelWindow;
+class TopLevelWindowContext;
+
+class TopLevelWindowPrivate : public UiComponentPrivate
+{
+  public:
+
+    TopLevelWindowPrivate();
+
+    virtual ~TopLevelWindowPrivate();
+
+    bool updateMenuBar();
+
+    /**
+     * Helper function used to pass the this pointer of the TopLevelWindow to
+     * its d-pointer.
+     *
+     * @param[in] window the this pointer of the TopLevelWindow that will be
+     * passed to its d-pointer.
+     */
+    void setTopLevelWindow(TopLevelWindow * window);
+
+    std::auto_ptr<MenuBar> menuBar;
+
+    /**
+     * The owner of this TopLevelWindow or nullptr if this TopLevelWindow does
+     * not have an owner.
+     */
+    TopLevelWindow * owner;
+
+    std::auto_ptr<TopLevelWindowContext> topLevelWindowContext;
+};
+#endif /* UI_PRIVATE_WINDOWS_TOPLEVELWINDOWPRIVATE_H_ */
