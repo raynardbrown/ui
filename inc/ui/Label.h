7c4e6221403fdec803dc3f3a6f942dbeeb52b421
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-03-06 23:43:34 -0600

    feat: Add the Label header

---
+++ inc/ui/Label.h
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: Label.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_LABEL_H_
+#define UI_LABEL_H_
+
+#include "ui/UiComponent.h"
+
+class AbstractColorChangeObserver;
+
+class LabelPrivate;
+
+class Label : public UiComponent
+{
+  public:
+
+    enum TextAlignment
+    {
+      RightAlignment,
+      LeftAlignment,
+      CenterAlignment
+    };
+
+    enum AcceleratorInterpretation
+    {
+      EnableAcceleratorInterpretation,
+      DisableAcceleratorInterpretation,
+    };
+
+    Label(const std::string& text,
+          TextAlignment alignment,
+          AcceleratorInterpretation acceleratorInterpretation);
+
+    virtual ~Label();
+
+    std::string getText() const;
+
+    void setText(const std::string& text);
+
+    void setColorChangeObserver(AbstractColorChangeObserver * colorChangeObserver);
+
+  protected:
+
+    virtual void internalCreate();
+
+    virtual void initializeBackgroundColor();
+
+    virtual void calculateInternalPreferredSize();
+
+    Label(LabelPrivate& d);
+};
+#endif /* UI_LABEL_H_ */
