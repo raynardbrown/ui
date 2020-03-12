5d9fd4f8723867f4d0777a58b4f2c327f537bae3
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-03-12 00:21:27 -0500

    feat: Add the MenuPrivate header

---
+++ src/ui/private/windows/MenuPrivate.h
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: MenuPrivate.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_PRIVATE_WINDOWS_MENUPRIVATE_H_
+#define UI_PRIVATE_WINDOWS_MENUPRIVATE_H_
+
+#include <string>
+
+#include <vector>
+
+#include "ui/private/UiComponentPrivate.h"
+
+class MenuItem;
+
+class MenuPrivate : public UiComponentPrivate
+{
+  public:
+
+    MenuPrivate(const std::string& text);
+
+    virtual ~MenuPrivate();
+
+    // the menu text
+    std::string text;
+
+    std::vector<MenuItem *> menuItems;
+};
+#endif /* UI_PRIVATE_WINDOWS_MENUPRIVATE_H_ */
