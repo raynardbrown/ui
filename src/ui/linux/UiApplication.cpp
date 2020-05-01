75fa2913210f5a589637a1760c935e9403fa9b49
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-04-30 23:47:32 -0500

    feat: Add UiApplication source (Linux)

---
+++ src/ui/linux/UiApplication.cpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiApplication.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <gtk/gtk.h>
+
+#include "ui/UiApplication.h"
+
+/* static */ int UiApplication::run(Window * window)
+{
+  gtk_main();
+  return 0;
+}
+
+/* static */ void UiApplication::quit()
+{
+  ::gtk_main_quit();
+}
