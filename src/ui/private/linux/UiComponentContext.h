c23ccca20f93b808ae7295d50b39ef98b0842310
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-05-06 23:42:24 -0500

    feat: Add the UiComponentContext header (Linux)

---
+++ src/ui/private/linux/UiComponentContext.h
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiComponentContext.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_PRIVATE_LINUX_UICOMPONENTCONTEXT_H_
+#define UI_PRIVATE_LINUX_UICOMPONENTCONTEXT_H_
+
+class UiComponentContext
+{
+  public:
+
+    UiComponentContext();
+
+    ~UiComponentContext();
+};
+#endif /* UI_PRIVATE_LINUX_UICOMPONENTCONTEXT_H_ */
