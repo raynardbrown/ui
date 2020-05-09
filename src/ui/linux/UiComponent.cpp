8ae8af46e0c934428d8484e94f28b8a458bf1224
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-05-08 23:29:14 -0500

    feat: Add UiComponent source (Linux)

---
+++ src/ui/linux/UiComponent.cpp
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiComponent.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "graphics/Color.h"
+#include "graphics/Font.h"
+#include "graphics/FontFactory.h"
+#include "graphics/Rectangle2D.h"
+#include "graphics/Size2D.h"
+
+#include "ui/TopLevelWindow.h"
+
+#include "ui/layout/Margin.h"
+#include "ui/layout/Padding.h"
+#include "ui/layout/ILayout.h"
+
+#include "ui/private/DefaultFocusImpl.h"
+#include "ui/private/IdManager.h"
+#include "ui/private/UiComponentPrivate.h"
+
+#include "ui/UiComponent.h"
